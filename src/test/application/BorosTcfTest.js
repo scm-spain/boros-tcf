2acc3c896f0631bc52ad48f0c61b52c850b637f4
maryam.malek <maryam.malek@adevinta.com>
2020-06-02 15:06:51 +0200

    add two consents test

--- src/test/application/BorosTcfTest.js
+++ src/test/application/BorosTcfTest.js
@@ -106,6 +106,55 @@ describe('BorosTcf', () => {
           expect(userConsent.vendorLegitimateInterests.has(1)).to.be.false
         })
     })
+    it('should generate and save correctly the new user consent when an old one exists', () => {
+      /* This test is not representing a real case. When the getVendorListUseCase is refactored 
+      this test should have a saved user consent with an old VendorList and then save a modified 
+      user consent with the latest vendorList version.
+       */
+      const givenPurpose2 = {
+        consents: {
+          '1': false,
+          '2': true
+        },
+        legitimateInterests: {
+          '1': false,
+          '2': true
+        }
+      }
+      const givenVendor2 = {
+        consents: {
+          '1': true,
+          '2': true
+        },
+        legitimateInterests: {
+          '1': true,
+          '2': true
+        }
+      }
+      return borosTcf
+        .saveUserConsent({purpose: givenPurpose, vendor: givenVendor})
+        .then(() => {
+          return borosTcf.saveUserConsent({
+            purpose: givenPurpose2,
+            vendor: givenVendor2
+          })
+        })
+        .then(() => {
+          const savedConsent = cookieStorageMock.storage.get('euconsent-v2')
+          expect(savedConsent).to.be.a('string')
+
+          const userConsent = TCString.decode(savedConsent)
+          expect(userConsent.cmpId).to.equal(BOROS_TCF_ID)
+          expect(userConsent.vendorConsents.has(1)).to.be.true
+          expect(userConsent.vendorConsents.has(2)).to.be.true
+          expect(userConsent.vendorLegitimateInterests.has(1)).to.be.true
+          expect(userConsent.vendorLegitimateInterests.has(2)).to.be.true
+          expect(userConsent.purposeConsents.has(1)).to.be.false
+          expect(userConsent.purposeConsents.has(2)).to.be.true
+          expect(userConsent.purposeLegitimateInterests.has(1)).to.be.false
+          expect(userConsent.purposeLegitimateInterests.has(2)).to.be.true
+        })
+    })
   })
 
   describe('loadUserConsent', () => {
