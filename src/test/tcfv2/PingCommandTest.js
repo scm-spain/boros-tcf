292bb66a9856880328ff25a2605fc622521a0c3a
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-06-10 23:35:46 +0200

    add test

--- src/test/tcfv2/PingCommandTest.js
+++ src/test/tcfv2/PingCommandTest.js
@@ -2,6 +2,11 @@ import 'jsdom-global/register'
 import {expect} from 'chai'
 import {PingReturn} from '../../main/domain/ping/PingReturn'
 import {TestableTcfApiInitializer} from '../testable/infrastructure/bootstrap/TestableTcfApiInitializer'
+import {GVLFactory} from '../../main/infrastructure/repository/iab/GVLFactory'
+import {TestableGVLFactory} from '../testable/infrastructure/repository/iab/TestableGVLFactory'
+import {VendorListValue} from '../fixtures/vendorlist/VendorListValue'
+import {CookieStorage} from '../../main/infrastructure/repository/cookie/CookieStorage'
+import {TestableCookieStorageMock} from '../testable/infrastructure/repository/TestableCookieStorageMock'
 
 describe('ping', () => {
   const command = 'ping'
@@ -11,6 +16,47 @@ describe('ping', () => {
     window.__tcfapi(command, version, (pingReturn, success) => {
       expect(success).to.be.true
       expect(pingReturn).to.be.instanceOf(PingReturn)
+    })
+  })
+
+  it('should return fixed settings', () => {
+    const givenCmpId = 129
+    TestableTcfApiInitializer.create().init()
+    window.__tcfapi(command, version, pingReturn => {
+      expect(pingReturn.gdprApplies).to.be.true
+      expect(pingReturn.cmpId).to.be.equal(givenCmpId)
+    })
+  })
+
+  it('should return gvlVersion', async () => {
+    const givenPurpose = {
+      consents: {},
+      legitimateInterests: {}
+    }
+    const givenVendor = {
+      consents: {},
+      legitimateInterests: {}
+    }
+    const givenVersion = 2
+    const cookieStorageMock = new TestableCookieStorageMock()
+    const borosTcf = TestableTcfApiInitializer.create()
+      .mock(CookieStorage, cookieStorageMock)
+      .init()
+    await borosTcf.saveUserConsent({purpose: givenPurpose, vendor: givenVendor})
+    await borosTcf.getVendorList()
+    const givenVendorListCommand = 'getVendorList'
+    const {vendorList, success} = await new Promise(resolve =>
+      window.__tcfapi(
+        givenVendorListCommand,
+        givenVersion,
+        (vendorList, success) => {
+          resolve({vendorList, success})
+        }
+      )
+    )
+    expect(success).to.be.true
+    window.__tcfapi(command, version, pingReturn => {
+      expect(pingReturn.gvlVersion).to.be.equal(vendorList.vendorListVersion)
     })
   })
 })
