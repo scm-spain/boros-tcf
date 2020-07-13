fc1d645024f621116c8958be9beba5f6a3dcc51f
Alex Castells <alextremp@hotmail.com>
2020-07-13 14:39:45 +0200

    use the constant value for latest version

--- src/main/infrastructure/repository/iab/GVLFactory.js
+++ src/main/infrastructure/repository/iab/GVLFactory.js
@@ -1,5 +1,8 @@
 import {GVL} from '@iabtcf/core'
-import {VENDOR_LIST_DEFAULT_LANGUAGE} from '../../../core/constants'
+import {
+  VENDOR_LIST_DEFAULT_LANGUAGE,
+  VENDOR_LIST_LATEST_VERSION
+} from '../../../core/constants'
 
 export class GVLFactory {
   constructor({
@@ -7,11 +10,11 @@ export class GVLFactory {
     language = VENDOR_LIST_DEFAULT_LANGUAGE
   } = {}) {
     GVL.baseUrl = baseUrl
-    GVL.latestFilename = `LATEST?language=${language}`
+    GVL.latestFilename = `${VENDOR_LIST_LATEST_VERSION}?language=${language}`
     GVL.versionedFilename = `[VERSION]?language=${language}`
   }
 
-  create({version = 'LATEST'} = {}) {
+  create({version = VENDOR_LIST_LATEST_VERSION} = {}) {
     return new GVL(version)
   }
 }
