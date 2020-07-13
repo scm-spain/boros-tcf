b49bfc8827933d1e9da9cbd02f10d5c5b320c938
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-07-13 12:39:32 +0200

    refactor: remove unnecesary cmpStaus changing

--- src/main/application/services/vendorconsent/LoadUserConsentUseCase.js
+++ src/main/application/services/vendorconsent/LoadUserConsentUseCase.js
@@ -1,25 +1,20 @@
 import {inject} from '../../../core/ioc/ioc'
 import {LoadConsentService} from '../../../domain/consent/LoadConsentService'
 import {EventStatusService} from '../../../domain/service/EventStatusService'
-import {StatusRepository} from '../../../domain/status/StatusRepository'
-import {Status} from '../../../domain/status/Status'
 
 class LoadUserConsentUseCase {
   constructor({
     loadConsentService = inject(LoadConsentService),
-    eventStatusService = inject(EventStatusService),
-    statusRepository = inject(StatusRepository)
+    eventStatusService = inject(EventStatusService)
   } = {}) {
     this._loadConsentService = loadConsentService
     this._eventStatusService = eventStatusService
-    this._status = statusRepository.getStatus()
   }
 
   async execute({notify = false} = {}) {
     const consent = await this._loadConsentService.loadConsent()
     const consentDto = consent.toJSON()
     if (consentDto.valid) {
-      this._status.cmpStatus = Status.CMPSTATUS_LOADED
       this._eventStatusService.updateTCLoaded({notify})
     }
     return consentDto
