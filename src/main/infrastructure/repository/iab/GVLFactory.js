6e0e70530a468712be3e8c0a2979000bfc3008f9
Alex Castells <alextremp@hotmail.com>
2020-07-14 10:19:31 +0200

    Revert "control the cache ourselves"
    This reverts commit 36924941ea021ae9a553b396103972e9d2695c4f.

--- src/main/infrastructure/repository/iab/GVLFactory.js
+++ src/main/infrastructure/repository/iab/GVLFactory.js
@@ -12,13 +12,9 @@ export class GVLFactory {
     GVL.baseUrl = baseUrl
     GVL.latestFilename = `${VENDOR_LIST_LATEST_VERSION}?language=${language}`
     GVL.versionedFilename = `[VERSION]?language=${language}`
-    this._cached = new Map()
   }
 
   create({version = VENDOR_LIST_LATEST_VERSION} = {}) {
-    if (!this._cached.has(version)) {
-      this._cached.set(version, new GVL(version))
-    }
-    return this._cached.get(version)
+    return new GVL(version)
   }
 }
