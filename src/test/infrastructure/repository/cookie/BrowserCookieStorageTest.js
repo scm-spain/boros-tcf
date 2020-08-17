cbe4cf409ba6793e3a6906df7f798f307132a651
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-08-17 14:37:24 +0200

    test: improve test

--- src/test/infrastructure/repository/cookie/BrowserCookieStorageTest.js
+++ src/test/infrastructure/repository/cookie/BrowserCookieStorageTest.js
@@ -1,6 +1,6 @@
 import {BrowserCookieStorage} from '../../../../main/infrastructure/repository/cookie/BrowserCookieStorage'
 import {expect} from 'chai'
-import {JSDOM} from 'jsdom'
+import jsdom, {JSDOM} from 'jsdom'
 
 describe('BrowserCookieStorage Should', () => {
   it('Write And read a cookie', () => {
@@ -8,7 +8,6 @@ describe('BrowserCookieStorage Should', () => {
       url: 'http://example.com/'
     }).window
     const browserCookieStorage = new BrowserCookieStorage({
-      domain: 'example.com',
       window
     })
 
@@ -29,16 +28,43 @@ describe('BrowserCookieStorage Should', () => {
     expect(cookie).equal('foo')
   })
   it('Write And read a cookie with different domain should be undefined', () => {
-    const window = new JSDOM('<!DOCTYPE html><body></body>', {
-      url: 'http://example.com/'
-    }).window
-    const browserCookieStorage = new BrowserCookieStorage({
-      domain: 'example2.com',
-      window
+    const givenCookie = 'foo'
+    const dom1 = new JSDOM('<!DOCTYPE html><body></body>', {
+      url: 'http://example1.com/'
     })
-
-    browserCookieStorage.save({data: 'foo'})
-    const cookie = browserCookieStorage.load()
-    expect(cookie).equal(undefined)
+    const browserCookieStorage1 = new BrowserCookieStorage({
+      window: dom1.window
+    })
+    const dom2 = new JSDOM('<!DOCTYPE html><body></body>', {
+      url: 'http://example2.com/'
+    })
+    const browserCookieStorage2 = new BrowserCookieStorage({
+      window: dom2.window
+    })
+    browserCookieStorage1.save({data: givenCookie})
+    const cookie1 = browserCookieStorage1.load()
+    const cookie2 = browserCookieStorage2.load()
+    expect(cookie1).to.equal(givenCookie)
+    expect(cookie2).to.be.undefined
+  })
+  it('Should keep the cookie between subdomains', () => {
+    const givenHost = 'example.com'
+    const givenCookie = 'foo'
+    const cookieJar = new jsdom.CookieJar()
+    const dom = new JSDOM('<!DOCTYPE html><body></body>', {
+      url: `http://www.${givenHost}/`,
+      cookieJar
+    })
+    const browserCookieStorage1 = new BrowserCookieStorage({
+      window: dom.window
+    })
+    browserCookieStorage1.save({data: givenCookie})
+    dom.reconfigure({url: `http://asubdomainof.${givenHost}/`})
+    const browserCookieStorage2 = new BrowserCookieStorage({
+      window: dom.window
+    })
+    const cookie2 = browserCookieStorage2.load()
+    expect(cookie2).to.equal(givenCookie)
+    expect(cookieJar.getCookiesSync(`http://${givenHost}/`)).to.have.lengthOf(1)
   })
 })
