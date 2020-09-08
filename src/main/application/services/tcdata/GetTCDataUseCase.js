3692a60e4d0771f8be36ca8cb92176933d9d2f91
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-09-08 14:14:04 +0200

    fix: negate condition in GetTCDataUseCase

--- src/main/application/services/tcdata/GetTCDataUseCase.js
+++ src/main/application/services/tcdata/GetTCDataUseCase.js
@@ -30,7 +30,7 @@ export class GetTCDataUseCase {
   execute({vendorIds} = {}) {
     if (
       Array.isArray(vendorIds) &&
-      vendorIds.some(vendorId => this._isInteger(vendorId) || vendorId < 1)
+      vendorIds.some(vendorId => !this._isInteger(vendorId) || vendorId < 1)
     ) {
       throw Error(
         'vendorIds parameter of getTCData has invalid data. It must be an array of positive integers, or undefined'
