1ddec5387c0b8256622a20326dcfe040ad825226
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-09-10 11:47:40 +0200

    fix: add more polyfills

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,9 @@
 import 'core-js/features/number/is-integer'
 import 'core-js/es/set'
 import 'core-js/features/string/repeat'
+import 'core-js/features/object/values'
+import 'core-js/features/string/includes'
+import 'core-js/features/array/includes'
 import 'regenerator-runtime/runtime'
 import {TcfApiInitializer} from './infrastructure/bootstrap/TcfApiInitializer'
 
