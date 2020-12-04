bafbf47569222297538b2cf2b37341e2cfb51824
Alex Castells <alextremp@hotmail.com>
2020-12-05 00:46:27 +0100

    update to last container version

--- src/test/testable/infrastructure/bootstrap/TestableTcfApiInitializer.js
+++ src/test/testable/infrastructure/bootstrap/TestableTcfApiInitializer.js
@@ -1,12 +1,11 @@
-import {iocModule, iocReset} from 'brusc'
-import {IOC_MODULE} from '../../../../main/core/ioc/ioc'
+import {inject} from '../../../../main/core/ioc/ioc'
 import {TcfApiInitializer} from '../../../../main/infrastructure/bootstrap/TcfApiInitializer'
 import {GVLFactory} from '../../../../main/infrastructure/repository/iab/GVLFactory'
 import {TestableGVLFactory} from '../repository/iab/TestableGVLFactory'
 
 class TestableTcfApiInitializer {
   constructor() {
-    this._mocks = new Map()
+    this._mocks = {}
   }
 
   static create() {
@@ -14,7 +13,7 @@ class TestableTcfApiInitializer {
   }
 
   mock(key, instance) {
-    this._mocks.set(key, instance)
+    this._mocks[key] = () => instance
     return this
   }
 
@@ -25,22 +24,15 @@ class TestableTcfApiInitializer {
       () => new TestableGVLFactory({language, latestGvlVersion})
     )
 
-    iocReset(IOC_MODULE)
-    iocModule({
-      module: IOC_MODULE,
-      initializer: ({singleton}) => {
-        Array.from(this._mocks.entries()).forEach(([key, instance]) =>
-          singleton(key, () => instance)
-        )
-      },
-      chain: true
-    })
+    inject.defaults = {
+      ...this._mocks
+    }
     return TcfApiInitializer.init({language, reporter, scope})
   }
 
   _keepOrAdd(mockKey, mockInitializer) {
-    if (!this._mocks.has(mockKey)) {
-      this._mocks.set(mockKey, mockInitializer())
+    if (!this._mocks[mockKey]) {
+      this._mocks[mockKey] = mockInitializer
     }
   }
 }
